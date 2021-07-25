1919d5fa39e060d6b8cffe68d8b59628032a0c9e
pedromn0 <muzzioneri@gmail.com>
2021-07-25 21:01:22 +0000

    Feat: create fetchPokemon function which allow work with a Pokemon object

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+// Function to Start the game. It will reunite generate PokeId, Fetch Pokemon
+
 // Function to ramdomize the 10 id and insert it each of the previous function
 let pokeArray = [];
 
@@ -9,6 +11,57 @@ function generatePokeId () {
 }
 
 generatePokeId ();
+
+// function to fetch one Pokemon and create an object
+function fetchPokemon () {
+    const url = `https://pokeapi.co/api/v2/pokemon/1`; 
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            const pokemon = {
+                id: data.id,
+                name: data.name,
+                sprites: data.sprites[`front_default`],
+                types: data.types
+            }
+            console.log(pokemon);
+        });
+};
+
+fetchPokemon ();
+
+// function to fetch eleven Pokemon and create an object of each of them
+
+let idArray = [];
+let endpointArray=[];
+
+function randomPokeArray () {
+    for (let i = 0; i < 11; i++) {
+        idArray.push(Math.floor(Math.random()*897)+1);
+    }
+    idArray.forEach(id => endpointArray.push(`https://pokeapi.co/api/v2/pokemon/${id}`));
+    console.log(endpointArray);
+}
+
+randomPokeArray ();
+
+function fetchElevenPokemon (endpointArray) {
+    let url;
+    endpointArray.forEach(url)
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            const pokemon = {
+                id: data.id,
+                name: data.name,
+                sprites: data.sprites[`front_default`],
+                types: data.types
+            }
+            console.log(pokemon);
+        });
+};
+
+fetchElevenPokemon ();
 
 // function to get the image
 function getPokemonId () {
